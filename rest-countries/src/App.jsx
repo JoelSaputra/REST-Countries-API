1b274f0a65648b77ca7aaa051feae8a124667516
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-06-17 17:54:45 -0400

    Finished the card component for all the data.json but still non-active

--- rest-countries/src/App.jsx
+++ rest-countries/src/App.jsx
@@ -7,11 +7,10 @@ import data from './data.json'
 
 function App() {
 
-  const [country, setCountry] = useState(data[0]);
+  const [countries, setCountries] = useState(data);
   return (
-    
 
-    <div className="min-h-screen bg-custom-white">
+    <div className="min-h-screen bg-custom-white overflow-x-hidden">
       <TopBar/>
 
       <div className="mt-20 ml-15 flex flex-row space-x-170">
@@ -20,8 +19,9 @@ function App() {
       </div>
 
 
-      <div className="mt-10 ml-15 mr-15">
-      <CountryCard country={country}/>
+      <div className="w-max mb-5 mt-10 ml-15 mr-15 grid grid-cols-4 space-x-32 space-y-32">
+       {countries.map((country)=> 
+       <CountryCard key={country.name} country={country}/> )}
       </div>
 
 
--- rest-countries/src/components/CountryCard.jsx
+++ rest-countries/src/components/CountryCard.jsx
@@ -4,12 +4,15 @@ import React from 'react'
 const CountryCard = ({ country }) => {
 
   return (
-    <div className="bg-stone-50 w-60 h-75 shadow-2xl rounded font-nunito">
+    <div className="bg-stone-50 w-60 h-80 shadow-2xl rounded font-nunito">
         
-        <img alt="flag" src={country.flags.svg}/>
+        <div className="w-60 h-40">
+        <img className="w-full h-full object-cover" alt="flag" 
+        src={country.flags?.svg || country.flags?.png || country.flag}/>
+        </div>
 
-        <div className="flex flex-col align-middle pl-5 pt-5">
 
+        <div className="flex flex-col align-middle pl-5 pt-5 pb-2">
         <h1 className="font-extrabold mb-4">{country.name}</h1>
         <p className="text-sm text-custom-grey">
             <span className="font-lightbold text-black">Population:</span> {country.population}
