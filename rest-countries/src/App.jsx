aa8e2d6e837f683b6049e1448a747fcc011431b1
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-06-17 19:00:37 -0400

    Updated feature on SearchBar

--- rest-countries/src/App.jsx
+++ rest-countries/src/App.jsx
@@ -8,19 +8,36 @@ import data from './data.json'
 function App() {
 
   const [countries, setCountries] = useState(data);
+  const [filteredCountries, setFilteredCountries] = useState(data);
+
+  const handleSearch = (searchTerm) => {
+    if(!searchTerm || searchTerm.trim() === ''){
+      setFilteredCountries(countries);
+      return;
+    }
+
+    const filtered = countries.filter((country) => 
+      country.name.toLowerCase().includes(searchTerm.toLowerCase())
+      
+    );
+
+    setFilteredCountries(filtered);
+
+  }
+
   return (
 
     <div className="min-h-screen bg-custom-white overflow-x-hidden">
       <TopBar/>
 
       <div className="mt-20 ml-15 flex flex-row space-x-170">
-      <SearchBar/>
+      <SearchBar onSearch={handleSearch}/>
       <OptionBar/>
       </div>
 
 
       <div className="w-max mb-5 mt-10 ml-15 mr-15 grid grid-cols-4 space-x-32 space-y-32">
-       {countries.map((country)=> 
+       {filteredCountries.map((country)=> 
        <CountryCard key={country.name} country={country}/> )}
       </div>
 
--- rest-countries/src/components/SearchBar.jsx
+++ rest-countries/src/components/SearchBar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
-const SearchBar = () => {
+const SearchBar = ({onSearch}) => {
+
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  }
+
+
+
   return (
-    <div className="bg-white flex align-text pl-10 pr-5
+    <form onSubmit={handleSubmit} className="bg-white flex align-text pl-10 pr-5
                       shadow-md max-w-md h-12 rounded">
           <FontAwesomeIcon icon={faMagnifyingGlass} className="text-gray-400 mt-4 mr-5"/>
         <input className="w-lg outline-none"
-        type="search" placeholder="Search for a country..."/>
-    </div>
+        type="search" 
+        placeholder="Search for a country..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        />
+    </form>
   )
 }
 
