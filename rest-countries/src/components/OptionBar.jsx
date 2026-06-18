96dbe739710bcb0a5d5e5599ae8be24904f23638
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-06-18 14:19:36 -0400

    OptionBar options non responsive update

--- rest-countries/src/components/OptionBar.jsx
+++ rest-countries/src/components/OptionBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useState } from 'react';
 
 const OptionBar = ({ countries }) => {
+
+  const [buttonOption, setButtonOption] = useState("Filter by Region");
+  const [isOpen, setIsOpen] = useState(false);
 
   const nonDuplicates = new Set(countries.map((country)=>{
     return country.region;
@@ -8,16 +12,38 @@ const OptionBar = ({ countries }) => {
   const nonDuplicatesArray = [...nonDuplicates];
 
 
-  return (
-    <div className="bg-white flex align-text pl-10 pr-5
-                      shadow-md max-w-md h-12 rounded">
+  const handleClickDisplay = () => {
+    if(isOpen){
+      setIsOpen(false);
+    }
+    else{
+      setIsOpen(true);
 
-    <select className="pr-5 outline-none bg-white">
-        <option>Filter by Region</option>
-        {nonDuplicatesArray.map((region)=>{
-          return <option key={region}>{region}</option>
-        })}
-    </select>
+    }
+    
+
+  }
+
+
+
+
+  return (
+    <div className="relative bg-white flex flex-col pl-10 pr-5
+                      shadow-md max-w-md h-12 rounded w-50">
+
+    <div className="pr-5 outline-none bg-white mb-3.5">
+        <button onClick={handleClickDisplay} className="mt-3">{buttonOption}</button>
+    </div>
+    
+    {isOpen &&  
+    <div className="absolute flex flex-col top-full left-0 
+                    mt-1 w-full bg-white shadow-lg 
+                    rounded-lg py-2 z-10 space-y-1">
+      {nonDuplicatesArray.map((region)=> {
+        return <button className="flex pl-10 mt-2" key={region}>{region}</button>
+      })}
+
+    </div> }
 
     </div>
   )
