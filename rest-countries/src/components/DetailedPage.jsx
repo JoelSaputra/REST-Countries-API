79470189cf8fe8ba7b21a4b727872c1d534ca73c
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-06-19 00:36:20 -0400

    Border-countries but non-responsive

--- rest-countries/src/components/DetailedPage.jsx
+++ rest-countries/src/components/DetailedPage.jsx
@@ -26,8 +26,65 @@ const DetailedPage = ({country}) => {
 </button>
         </div>
 
-    <div className="w-120 mt-15 ml-11 ">
-        <img alt="flag-logo" src={country.flags.svg}/>
+    <div className="h-90 flex flex-row space-x-30">
+        <div className="w-120 mt-15 ml-11 h-screen">
+            <img alt="flag-logo" src={country.flags.svg} className=""/>
+        </div>
+
+       <div className="mt-20">
+  <h1 className="font-bold text-3xl mb-6">{country.name}</h1>
+  
+  <div className="grid grid-cols-2 gap-x-50 gap-y-2 mt-10">
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Native Name:</span>
+      {country.nativeName}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Population:</span>
+      {country.population?.toLocaleString()}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Region:</span>
+      {country.region}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Sub Region:</span>
+      {country.subregion}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Capital:</span>
+      {country.capital}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Top Level Domain:</span>
+      {country.topLevelDomain}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Currencies:</span>
+      {country.currencies?.[0]?.code || 'N/A'}
+    </p>
+    <p className="text-sm">
+      <span className="font-bold mr-1.5">Languages:</span>
+      {country.languages.map(lang => lang.name).join(', ') || 'N/A'}
+    </p>
+  </div>
+
+  <div className="mt-12 flex flex-wrap items-center gap-2">
+    <p className="font-semibold mr-1">Border Countries:</p>
+    {country.borders.length > 0 ? (
+        country.borders.map((border) => (
+            <span key={border} className="px-3 py-1 bg-white shadow-sm rounded text-sm hover:cursor-pointer">
+                {border}
+            </span>
+        ))
+    ) : (
+        <span className="text-gray-500">None</span>
+    )}
+</div>
+</div>
+
+
+
 
     </div>
 
